492abd91d44754ec3d1e80a0f31f814c24b610bc
i11090 <i11090@student.htlwrn.ac.at>
2015-11-20 08:39:07 +0100

    Ueberpruefung der Eingabe wurde vorgenommen

--- Schweiger/BMI_Rechner/app/src/main/java/com/example/michi/bmi_rechner/MainActivity.java
+++ Schweiger/BMI_Rechner/app/src/main/java/com/example/michi/bmi_rechner/MainActivity.java
@@ -1,11 +1,67 @@
 package com.example.michi.bmi_rechner;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void onClickCalculate(View button){
+        final EditText input_weight = (EditText) findViewById(R.id.body_weight);
+        final EditText input_height = (EditText) findViewById(R.id.body_height);
+        final EditText input_age = (EditText) findViewById(R.id.age);
+        final EditText input_name = (EditText) findViewById(R.id.name);
+
+        String weight = input_weight.getText().toString();
+        String height = input_height.getText().toString();
+        String age = input_age.getText().toString();
+        String name = input_name.getText().toString();
+
+        check_input(weight, height);
+
+
+    }
+
+    public  boolean check_input(String weight, String height){
+        if (weight.equals("") || height.equals("")){
+            provide_Toast("Es muss eine Eingabe für das Gewicht und die Höhe vorhandne sein!", "error");
+            return false;
+        }
+        else{
+            try{
+              Float.parseFloat(weight);
+                try{
+                    Float.parseFloat(height);
+                    return true;
+                }
+                catch (Exception e){
+                    provide_Toast("Die Eingabe für die Höhe ist ungültig", "error");
+                    return false;
+                }
+            }
+            catch (Exception e){
+                provide_Toast("Die Eingabe für das Gewicht ist ungültig", "errror");
+                return false;
+            }
+        }
+    }
+
+    public void provide_Toast(String message, String kind_of_message){
+        Toast toast = Toast.makeText(this, message, Toast.LENGTH_SHORT);
+        View view_toast = toast.getView();
+        if (kind_of_message.equals("error")) { //Wenn error, dann Ausgabe des Toasts in roter Frabe
+            view_toast.setBackgroundColor(Color.RED);
+        }
+        else if(kind_of_message.endsWith("information")){ // Wenn information, dann Ausgabe des Toasts in grüner Farbe
+            view_toast.setBackgroundColor(Color.GREEN);
+        }
+        toast.show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
